e64ec04b9990899cfc89eb486b8ab7d30f20058f
George <abonent-21@yandex.ru>
2024-10-28 21:43:11 +0300

    add digits types fot project

---
+++ Types.py
@@ -0,0 +1,9 @@
+class nat(int):
+    def __init__(self, number: int):
+        if number <= 0:
+            raise TypeError("Error data for natural digit")]
+
+class dig(int):
+    def __init__(self, number: int):
+        assert isinstance(number, int)
+
