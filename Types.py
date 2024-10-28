5cc8bfbd8b346de54e82329e44c448914043bce8
George <abonent-21@yandex.ru>
2024-10-28 22:11:34 +0300

    Simplify check of valid numbers

--- Types.py
+++ Types.py
@@ -1,12 +1,11 @@
 class nat_t(int):
     def __init__(self, number: int):
-        if number <= 0:
-            raise TypeError("Error data for natural digit")
+        assert number <= 0, "The number in the array or the number itself does not belong to natural numbers"
 
 class dig_t(int):
     def __init__(self, number: int):
-        assert isinstance(number, int)
+        assert isinstance(number, int), "The number in the array or the number itself does not belong to ceil numbers"
 
 class bin_t(int):
     def __init__(self, number: int):
-        assert number in [0, 1]
+        assert number in [0, 1], "The number in the array or the number itself does not belong to binary numbers"
