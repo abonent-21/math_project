e91d080fcd69a5f1398995576feaf9e76d543d15
George <abonent-21@yandex.ru>
2024-10-29 00:33:25 +0300

    Add new types of numbers

--- Types.py
+++ Types.py
@@ -9,12 +9,33 @@ class nat_0:
 
         self.array = array
         self.n = n
+    
+    def __eq__(self, other):
+        # Проверяем, что другой объект - это тоже nat_0 и имеет те же значения атрибутов
+        if isinstance(other, nat_0):
+            return self.array == other.array and self.n == other.n
+        return False
+
+    def __repr__(self):
+        return f"nat_0({self.array}, {self.n})"
+
+
 
 class dig:
     def __init__(self, number: int):
         assert 0 <= number <= 9
 
         self.array = number
+    
+    def __eq__(self, other):
+        # Проверяем, что другой объект - это тоже dig и имеет то же значение
+        if isinstance(other, dig):
+            return self.value == other.value
+        return False
+
+    def __repr__(self):
+        # Удобное представление для вывода информации об объекте
+        return f"dig({self.value})"
 
 class ceil:
     def __init__(self, array: list, n: int, sign: int):
@@ -25,4 +46,13 @@ class ceil:
         self.array = array
         self.n = n
         self.sign = sign
-        
+
+    def __eq__(self, other):
+        # Проверяем, что другой объект - это тоже ceil и имеет те же значения атрибутов
+        if isinstance(other, ceil):
+            return self.array == other.array and self.n == other.n and self.sign == other.sign
+        return False
+
+    def __repr__(self):
+        # Удобное представление для вывода информации об объекте
+        return f"ceil({self.array}, {self.n}, {self.sign})"
