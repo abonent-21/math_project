8a2d0331626d25e8f3a22cd3d49867e57e3e4135
3381_george_kozlov <85515455+abonent-21@users.noreply.github.com>
2024-10-31 23:09:49 +0300

    Update MUL_ZM_Z.py

--- MUL_ZM_Z.py
+++ MUL_ZM_Z.py
@@ -1,16 +1,19 @@
-from Types import ceil
-
+from Types import ceil  # Импортируем класс ceil из модуля Types
 
 def MUL_ZM_Z(number: ceil) -> ceil:
     """
-    Умножение целого на (-1)
+    Функция для умножения целого числа на (-1).
+    
+    Параметры:
+    - number: объект класса ceil, представляющий целое число с возможным знаком.
+    
+    Возвращает:
+    - Объект класса ceil с изменённым знаком числа.
     """
-
-    num = number.copy()
-    if num.array == [0]:
-        return num
-    num.sign = (num.sign + 1) % 2
-    return num
-
+    num = number.copy()  # Создаем копию переданного числа
+    if num.array == [0]:  # Если число равно 0
+        return num  # Возвращаем 0 без изменений
+    num.sign = (num.sign + 1) % 2  # Меняем знак числа (0 на 1 и наоборот)
+    return num  # Возвращаем объект с изменённым знаком
 
     
