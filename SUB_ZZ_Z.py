4b824ebc8bbf6e9f7d286a6956668103413e77ee
Mark Markov <144445462+sxeDexs@users.noreply.github.com>
2024-11-02 23:49:06 +0300

    SUB_ZZ_Z & test fix

--- SUB_ZZ_Z.py
+++ SUB_ZZ_Z.py
@@ -5,6 +5,7 @@ from ADD_NN_N import ADD_NN_N  # Импортируем функцию для с
 from SUB_NN_N import SUB_NN_N  # Импортируем функцию для вычитания натуральных чисел
 from MUL_ZM_Z import MUL_ZM_Z  # Импортируем функцию для изменения знака целого числа
 from COM_NN_D import COM_NN_D
+from TRANS_Z_N import TRANS_Z_N
 
 
 def SUB_ZZ_Z(A: ceil, B: ceil) -> ceil:
@@ -26,24 +27,24 @@ def SUB_ZZ_Z(A: ceil, B: ceil) -> ceil:
     # Если A и B имеют одинаковый знак
     if sign_A == sign_B:
         # Вычисляем абсолютные значения
-        abs_A = ABS_Z_Z(A)
-        abs_B = ABS_Z_Z(B)
+        abs_A = TRANS_Z_N(ABS_Z_Z(A))
+        abs_B = TRANS_Z_N(ABS_Z_Z(B))
 
-        if COM_NN_D(abs_A.to_nat_0(), abs_B.to_nat_0()) == 2:  # A > B
-            result_array = SUB_NN_N(abs_A.to_nat_0(), abs_B.to_nat_0()).array
+        if COM_NN_D(abs_A, abs_B) == 2:  # A > B
+            result_array = SUB_NN_N(abs_A, abs_B).array
             result_sign = A.sign
         else:  # A <= B
-            result_array = SUB_NN_N(abs_B.to_nat_0(), abs_A.to_nat_0()).array
+            result_array = SUB_NN_N(abs_B, abs_A).array
             result_sign = 1 - A.sign
 
     # Если A и B имеют разные знаки
     elif sign_A == dig(2) and sign_B == dig(1):  # A положительное, B отрицательное
-        positive_B = MUL_ZM_Z(B)  # Меняем знак B
-        result_array = ADD_NN_N(A.to_nat_0(), positive_B.to_nat_0()).array
+        positive_B = TRANS_Z_N(MUL_ZM_Z(B))  # Меняем знак B
+        result_array = ADD_NN_N(TRANS_Z_N(A), positive_B).array
         result_sign = 0  # Результат положительный
     elif sign_A == dig(1) and sign_B == dig(2):  # A отрицательное, B положительное
-        positive_A = MUL_ZM_Z(A)  # Меняем знак A
-        result_array = ADD_NN_N(positive_A.to_nat_0(), B.to_nat_0()).array
+        positive_A = TRANS_Z_N(MUL_ZM_Z(A))  # Меняем знак A
+        result_array = ADD_NN_N(positive_A, TRANS_Z_N(B)).array
         result_sign = 1  # Результат отрицательный
 
     # Проверка на нулевой результат
