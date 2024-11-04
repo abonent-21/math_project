93954886cf1e436ee68250c55e43142cda0e184f
Maxim Beskonchin <mabeskonchin@stud.etu.ru>
2024-11-05 02:26:22 +0300

    Q5-Q8_Maks

---
+++ MUL_QQ_Q.py
@@ -0,0 +1,30 @@
+# MUL_QQ_Q.py
+# Умножение дробей
+# Автор: Бескончин М.А. Группа - ПМИ-3381
+
+from Types import rat, nat_0,ceil
+
+from MUL_ZZ_Z import MUL_ZZ_Z
+from TRANS_N_Z import TRANS_N_Z
+from RED_Q_Q import RED_Q_Q
+
+
+def MUL_QQ_Q(multiplier_one_accepted: rat, multiplier_two_accepted: rat) -> rat:
+    """
+        Функция умножает две дроби (рациональных числа) и возвращает результат в несократимой форме.
+
+        :param multiplier_one_accepted: Первое множитель (тип rat)
+        :param multiplier_two_accepted: Второе множитель (тип rat)
+        :return: Результат умножения двух дробей в несократимой форме (тип rat)
+    """
+
+    result = rat(num=nat_0([0], 1), den=ceil([0], 1, 0))
+    multiplier_one = multiplier_one_accepted.copy()
+    multiplier_two = multiplier_two_accepted.copy()
+
+    # Умножаем числители двух дробей
+    result.num = MUL_ZZ_Z(multiplier_one.num,multiplier_two.num)
+    # Умножаем знаменатели двух дробей
+    result.den = MUL_ZZ_Z(TRANS_N_Z(multiplier_one.den), TRANS_N_Z(multiplier_two.den))
+    # Сокращаем полученную дробь до несократимой формы
+    return RED_Q_Q(result)
