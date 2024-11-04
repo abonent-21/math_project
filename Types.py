d366185c1f63ea483ef3cc80a87b0cb927cf5984
NickolaySychev <sychev.nickolay@mail.ru>
2024-11-04 12:03:40 +0300

    fix

--- Types.py
+++ Types.py
@@ -4,7 +4,7 @@ class nat_0(object):
         assert all(0 <= i <= 9 for i in array), "Все элементы должны быть цифрами от 0 до 9"
         self.array = array
         self.n = n
-        
+    
     def __eq__(self, other):
         if isinstance(other, nat_0):
             return self.array == other.array and self.n == other.n
@@ -12,16 +12,16 @@ class nat_0(object):
 
     def __repr__(self):
         return f"nat_0({self.array}, {self.n})"
-
+    
     def copy(self):
-        return nat_0(self.array.copy(), self.n)
+        return nat_0(self.array, self.n)
 
 
 class dig(object):
     def __init__(self, number: int):
         assert 0 <= number <= 9, "Число должно быть цифрой от 0 до 9"
         self.value = number  # Исправлено на `self.value`
-
+    
     def __eq__(self, other):
         if isinstance(other, dig):
             return self.value == other.value
@@ -29,7 +29,7 @@ class dig(object):
 
     def __repr__(self):
         return f"dig({self.value})"  # Исправлено на `self.value`
-
+    
     def copy(self):
         return dig(self.value)
 
@@ -50,23 +50,6 @@ class ceil(object):
 
     def __repr__(self):
         return f"ceil({self.array}, {self.n}, {self.sign})"
-
+    
     def copy(self):
-        return ceil(self.array.copy(), self.n, self.sign)
-
-class rat(object):
-    def __init__(self, num: ceil, den: nat_0):
-        assert den.array != [0], "Знаменатель не может быть равен 0"
-        self.num = num
-        self.den = den
-
-    def __eq__(self, other):
-        if isinstance(other, rat):
-            return self.num == other.num and self.den == other.den
-        return False
-
-    def __repr__(self):
-        return f"rat({self.num}, {self.den})"
-
-    def copy(self):
-        return rat(self.num.copy(), self.den.copy())
+        return ceil(self.array, self.n, self.sign)
