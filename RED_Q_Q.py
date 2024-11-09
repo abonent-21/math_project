7101f3132cd6f0eabfe2dc07ef314b6d1f8d8609
derranos <lexa-kulakow@mail.ru>
2024-11-09 20:52:02 +0300

    Q1-4 func fix

--- RED_Q_Q.py
+++ RED_Q_Q.py
@@ -13,7 +13,7 @@ def RED_Q_Q(val: rat) -> rat:
     n1 = GCF_NN_N(natNum, val.den)
     n2 = n1.copy()
     # Делим числитель на НОД, чтобы сократить дробь
-    val.num = DIV_ZZ_Z(val.num, ceil(n1, n1.n, 0))
+    val.num = DIV_ZZ_Z(val.num, ceil(n1.array, n1.n, 0))
     
     # Делим знаменатель на НОД, чтобы сократить дробь
     val.den = DIV_NN_N(val.den, n2)
