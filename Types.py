77805dd0cdb35225d7db7859c573e2d49d385b8a
Mark Markov <144445462+sxeDexs@users.noreply.github.com>
2024-11-09 09:46:16 +0300

    ...

--- Types.py
+++ Types.py
@@ -4,7 +4,7 @@ class nat_0(object):
         assert all(0 <= i <= 9 for i in array), "Все элементы должны быть цифрами от 0 до 9"
         self.array = array
         self.n = n
-        
+
     def __eq__(self, other):
         if isinstance(other, nat_0):
             return self.array == other.array and self.n == other.n
@@ -53,3 +53,40 @@ class ceil(object):
 
     def copy(self):
         return ceil(self.array.copy(), self.n, self.sign)
+
+
+class rat(object):
+    def __init__(self, num: ceil, den: nat_0):
+        assert den.array != [0], "Знаменатель не может быть равен 0"
+        self.num = num
+        self.den = den
+
+    def __eq__(self, other):
+        if isinstance(other, rat):
+            return self.num == other.num and self.den == other.den
+        return False
+
+    def __repr__(self):
+        return f"rat({self.num}, {self.den})"
+
+    def copy(self):
+        return rat(self.num.copy(), self.den.copy())
+
+
+class pol(object):
+    def __init__(self, coefficients: list, m: int):
+        assert m == len(coefficients) - 1, "Степень многочлена должна быть на 1 меньше длины массива коэффициентов"
+        assert all(isinstance(coef, rat) for coef in coefficients), "Все элементы должны быть экземплярами класса rat"
+        self.coefficients = coefficients
+        self.m = m  # Явное указание степени многочлена
+
+    def __eq__(self, other):
+        if isinstance(other, pol):
+            return self.coefficients == other.coefficients and self.m == other.m
+        return False
+
+    def __repr__(self):
+        return f"pol({self.coefficients}, {self.m})"
+
+    def copy(self):
+        return pol(self.coefficients.copy(), self.m)
