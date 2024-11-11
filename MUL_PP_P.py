23e2707798f736ba020d0e2ce7c6bcc99b9ad3b5
NickolaySychev <sychev.nickolay@mail.ru>
2024-11-11 15:03:36 +0300

    fix

--- MUL_PP_P.py
+++ MUL_PP_P.py
@@ -15,21 +15,21 @@ def MUL_PP_P(arr1: pol, arr2: pol) -> pol:
     """
     st1 = DEG_P_N(arr1)  # Степень первого многочлена
     st2 = DEG_P_N(arr2)  # Степень второго многочлена
+    
     ans_st = ADD_NN_N(st1, st2) # Вычисляем степень итогового полинома
     ans_st = ADD_1N_N(ans_st) # Прибавляем единицу для правильного подсчета результата.
-    
     ans_len = int(''.join(map(str, ans_st.array))) # Переводим в int для правильной инициализации итогового массива
-    
+
     result = pol([rat(ceil([0], 1, 0), nat_0([1], 1)) for _ in range(ans_len)], ans_len - 1)
     inter_pol = []
-    
+
     ans_len2 = int(''.join(map(str, st2.array))) # Переводим для правильной работы цикла
-    
+
     for i in range(ans_len2 + 1):
         k = MUL_Pxk_P(arr1, nat_0([arr2.m - i], 1)) # Умножение на степень x^k
         e = MUL_PQ_P(k, arr2.coefficients[arr2.m - i]) # Умножение на коэффициент второго полинома
         inter_pol.append(e)  # Добавляем промежуточный результат в массив
-        
+
     # Проходим по каждому члену второго полинома
     for i in inter_pol:
         # Проверка и выравнивание длины полиномов
@@ -45,6 +45,5 @@ def MUL_PP_P(arr1: pol, arr2: pol) -> pol:
         # Теперь складываем коэффициенты с одинаковыми степенями
         for j in range(max(len_result, len_i)):
             result.coefficients[j] = ADD_QQ_Q(result.coefficients[j], i.coefficients[j])
-     
-    return result
 
+    return result
