cfae2cdfe114d6c3480b17c28c7baa7eb16d64ea
George <abonent-21@yandex.ru>
2024-11-14 19:32:44 +0300

    find error in mod_p_p using nmr_p_p

--- NMR_P_P.py
+++ NMR_P_P.py
@@ -12,26 +12,38 @@ def find_devisors(r: rat) -> list:
     moderator: ceil = int(''.join([str(i) for i in r.num.array])) # числитель
     denominator: nat_0 = int(''.join([str(i) for i in r.den.array])) # знаменатель
 
-    devisors = [] # список делительей
+    devisors_moderator = [] # список делителей числителя
 
     numbers = [int(j) for j in str(moderator)] # числитель
-    devisors.append(rat(ceil(numbers, len(numbers), 0), nat_0([1], 1))) # числитель + как делитель
-    devisors.append(rat(ceil(numbers, len(numbers), 1), nat_0([1], 1))) # числитель - как дклитель
+    devisors_moderator.append(rat(ceil(numbers, len(numbers), 0), nat_0([1], 1))) # числитель + как делитель
+    devisors_moderator.append(rat(ceil(numbers, len(numbers), 1), nat_0([1], 1))) # числитель - как дклитель
     for i in range(2, int(abs(moderator)**0.5 + 1) + 1): # все делители числителя
         if moderator % i == 0:
             numbers = [int(j) for j in str(i)]
-            devisors.append(rat(ceil(numbers, len(numbers), 0), nat_0([1], 1)))
-            devisors.append(rat(ceil(numbers, len(numbers), 1), nat_0([1], 1)))
+            devisors_moderator.append(rat(ceil(numbers, len(numbers), 0), nat_0([1], 1)))
+            devisors_moderator.append(rat(ceil(numbers, len(numbers), 1), nat_0([1], 1)))
     
+    devisors_denominator = [] # список делителей знаменателя
 
     numbers = [int(j) for j in str(denominator)] # знаменатель
-    devisors.append(rat(ceil([1], 1, 0), nat_0(numbers, len(numbers)))) # знаменатель + как делитель
-    devisors.append(rat(ceil([1], 1, 1), nat_0(numbers, len(numbers)))) # знаменатель - как делитель 
+    devisors_denominator.append(rat(ceil([1], 1, 0), nat_0(numbers, len(numbers)))) # знаменатель + как делитель
+    devisors_denominator.append(rat(ceil([1], 1, 1), nat_0(numbers, len(numbers)))) # знаменатель - как делитель 
     for i in range(2, int(abs(denominator)**0.5 + 1) + 1): # все делители знаменателя
         if denominator % i == 0:
             numbers = [int(j) for j in str(i)]
-            devisors.append(rat(ceil([1], 1, 0), nat_0(numbers, len(numbers))))
-            devisors.append(rat(ceil([1], 1, 1), nat_0(numbers, len(numbers))))
+            devisors_denominator.append(rat(ceil([1], 1, 0), nat_0(numbers, len(numbers))))
+            devisors_denominator.append(rat(ceil([1], 1, 1), nat_0(numbers, len(numbers))))
+
+    devisors = []
+
+    for i in devisors_moderator:
+        for j in devisors_denominator:
+            rat_num = rat(ceil(i.num.array, i.num.n, i.num.sign), nat_0(j.den.array, j.den.n))
+            rat_num = DIV_QQ_Q(rat_num, rat(ceil([1], 1, 0), nat_0([1], 1)))
+            if rat_num not in devisors:
+                devisors.append(rat_num)
+    
+    devisors += devisors_moderator + devisors_denominator
 
     return devisors
 
@@ -48,14 +60,14 @@ def NMR_P_P(polym: pol) -> pol:
     # Первоначальное преобразование. Пример: 7x^2 + x - 6 ---> x^2 + (1/7)x - 6/7 
     new_coefs = []
     for elem in polynom.coefficients:
-        new_coefs.append(DIV_QQ_Q(elem, polynom.coefficients[0])) # делим каждый элемент многочлена на старший коэфициент
+        new_coefs.append(DIV_QQ_Q(elem, polynom.coefficients[-1])) # делим каждый элемент многочлена на старший коэфициент
 
     polynom = pol(new_coefs, len(new_coefs) - 1)
     null_pol = pol([rat(ceil([0], 1, 0), nat_0([1], 1))], 0) # нулевой поленом
 
     polynoms = []
 
-    free_coef = polynom.coefficients[-1] # свободный коэфициент
+    free_coef = polynom.coefficients[0] # свободный коэфициент
     devisors = find_devisors(free_coef) # делители свободного коэфициента
     idx_div = 0 # индекс делителя в списке делителей
     while idx_div != len(devisors):
@@ -65,7 +77,7 @@ def NMR_P_P(polym: pol) -> pol:
                             ], 1)
         if MOD_PP_P(polynom, devisor_polym) == null_pol:
             polynom = DIV_PP_P(polynom, devisor_polym)
-            if polynom not in polynom:
+            if polynom not in polynoms:
                 polynoms.append(polynom)
         else:
             idx_div += 1
@@ -77,14 +89,15 @@ def NMR_P_P(polym: pol) -> pol:
 
 
 p5 = pol([
-        rat(ceil([6], 1, 0), nat_0([7], 1)),  # 6/7 * x^0
-        rat(ceil([1], 1, 0), nat_0([7], 1)),  # 1/7 * x^1
-        rat(ceil([1], 1, 0), nat_0([1], 1))   # 1 * x^2
-    ], 2)
+        rat(ceil([3, 6], 2, 0), nat_0([1], 1)),  # 36 * x^0
+        rat(ceil([2, 4], 2, 0), nat_0([1], 1)),  # 24 * x^1
+        rat(ceil([9, 5], 2, 0), nat_0([1], 1)),  # 95  * x^2
+        rat(ceil([6, 9], 2, 1), nat_0([1], 1)),  # -69 * x^3
+        rat(ceil([6, 3], 2, 1), nat_0([1], 1)),  # -63 * x^4
+        rat(ceil([4, 9], 2, 0), nat_0([1], 1))   # 49 * x^5
+    ], 5) # 49x^5 - 63x^4 - 69*x^3 + 95*x^2 + 24*x - 36 = (7x + 6)^2 * (x - 1)^3
 
-p2 = pol([
-        rat(ceil([6], 1, 1), nat_0([7], 1)),  # -6/7 * x^0
-        rat(ceil([1], 1, 0), nat_0([1], 1))   # 1 * x^1
-    ], 1)
+a = pol([rat(ceil([6], 1, 0), nat_0([1], 1)), rat(ceil([7], 1, 0), nat_0([1], 1))], 1)
+# 7x + 6
 
-print(find_devisors(rat(ceil([6], 1, 1), nat_0([7], 1))))
+print(MOD_PP_P(p5, a)) # pol([rat(ceil([7, 2], 2, 0), nat_0([1], 1))], 0)
