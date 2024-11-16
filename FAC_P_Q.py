31eb55f8ef80b00c2b4a02c2b06f5e05f27e602d
Mark Markov <144445462+sxeDexs@users.noreply.github.com>
2024-11-16 11:25:55 +0300

    FAC_P_Q fix

--- FAC_P_Q.py
+++ FAC_P_Q.py
@@ -5,7 +5,8 @@ from ABS_Z_Z import ABS_Z_Z
 from GCF_NN_N import GCF_NN_N
 from LCM_NN_N import LCM_NN_N
 from TRANS_N_Z import TRANS_N_Z
-from DIV_NN_N import DIV_NN_N
+from TRANS_Z_N import TRANS_Z_N
+from RED_Q_Q import RED_Q_Q
 
 
 def FAC_P_Q(p: pol) -> rat:
@@ -14,33 +15,31 @@ def FAC_P_Q(p: pol) -> rat:
     Эта функция нужна для упрощения полинома.
     Мы выносим полученное значение за скобки, получая удобный для решения полином.
     """
-    
+
     # Получаем числители из коэффициентов (рациональные числа)
     # Переводим их в натуральные числа, так как НОД - натуральное число.
-    numerators = [ABS_Z_Z(coeff.num) for coeff in p.coefficients] 
-    
+    numerators = [TRANS_Z_N(ABS_Z_Z(coeff.num)) for coeff in p.coefficients]
+
     # Получаем знаменатели из коэффициентов (натуральные числа).
-    denominators = [coeff.den for coeff in p.coefficients] 
-    
+    denominators = [coeff.den for coeff in p.coefficients]
+
     # Определяем начальное значение для НОДа как первый числитель.
-    gcd = numerators[0] 
-    
+    gcd = numerators[0]
+
     # Если коэффициентов больше одного, вычисляем НОД всех числителей.
-    if p.m > 1: 
+    if p.m > 1:
         for i in numerators[1:]:
             gcd = GCF_NN_N(gcd, i)
-    
+
     # Проводим те же самые действия для НОКа.
     lcm = denominators[0]
-    if p.m > 1: # если коэффицентов полинома больше 1, то на каждой итерации вычисляем НОК. 
-        for i in denominators[1:]: 
+    if p.m > 1:  # если коэффицентов полинома больше 1, то на каждой итерации вычисляем НОК.
+        for i in denominators[1:]:
             lcm = LCM_NN_N(lcm, i)
-            
-    # На выходе имеем дробь. Сделаем проверку на то, сократима она или нет.
-    common_gcd = GCF_NN_N(gcd, lcm)  # Находим общий НОД для числителя и знаменателя
-    gcd = DIV_NN_N(gcd, common_gcd) 
-    lcm = DIV_NN_N(lcm, common_gcd) 
-    
-    gcd = TRANS_N_Z(gcd) # Перевод значения НОДа в целое, т.к. на выходе рац. число
-    
-    return rat(gcd, lcm) 
+
+    gcd = TRANS_N_Z(gcd)  # Перевод значения НОДа в целое, т.к. на выходе рац. число
+
+    # Сокращаем дробь, если она сократимая.
+    result = RED_Q_Q(rat(gcd, lcm))
+
+    return result
