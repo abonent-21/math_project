71d1437155a5cf3cfea4d589f19a955e87a5fd61
Mark Markov <144445462+sxeDexs@users.noreply.github.com>
2024-11-16 12:13:10 +0300

    DER_P_P fix

--- DER_P_P.py
+++ DER_P_P.py
@@ -1,4 +1,4 @@
-# Автор Козлов Георгий группа 3381
+# Автор: Козлов Г.Е. Группа - ПМИ-3381
 
 from Types import pol, rat, ceil, nat_0
 from MUL_QQ_Q import MUL_QQ_Q
@@ -12,18 +12,18 @@ def DER_P_P(polym: pol) -> pol:
     # Коэффициенты производной
     derived_coeffs = []
 
-    null_pol = pol([rat(ceil([0], 1, 0), nat_0([1], 1))], 0) # нулевой поленом
+    null_pol = pol([rat(ceil([0], 1, 0), nat_0([1], 1))], 0)  # нулевой поленом
 
-    if polym == null_pol: # если полином нулевой, но возвращаем, нулевой полином
+    if polym == null_pol:  # если полином нулевой, но возвращаем, нулевой полином
         return null_pol
     # Перебираем коэффициенты исходного многочлена
     for i, coef in enumerate(polym.coefficients):
         if i > 0:  # Первая степень (i = 0) в производной исчезает
-            mul_coef = rat(ceil([i], 1, 0), nat_0([1], 1))
+            i_array = [int(x) for x in str(i)]
+            mul_coef = rat(ceil(i_array, len(i_array), 0), nat_0([1], 1))
 
             elem = MUL_QQ_Q(mul_coef, coef)
             derived_coeffs.append(elem)
 
     # Степень производной будет на 1 меньше, чем у исходного многочлена
     return pol(derived_coeffs, polym.m - 1)
-
